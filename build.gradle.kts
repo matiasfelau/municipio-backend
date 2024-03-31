2ccc5c9f6375474db49aab0edd8ffc9606806f42
matiasfelau <matiasfelau@gmail.com>
2024-03-30 21:33:22 -0300

    Creando el repositorio

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.23"
+    id("io.ktor.plugin") version "2.3.9"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23"
+}
+
+group = "ar.edu.uade"
+version = "0.0.1"
+
+application {
+    mainClass.set("ar.edu.uade.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    //database
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.exposed:exposed-core:0.35.1")
+    implementation("org.jetbrains.exposed:exposed-dao:0.35.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.35.1")
+    implementation("mysql:mysql-connector-java:8.0.25")
+
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ src/main/kotlin/ar/edu/uade/models/Rubro.kt
@@ -0,0 +1,13 @@
+package ar.edu.uade.models
+
+import org.jetbrains.exposed.sql.Table
+
+data class Rubro(val idRubro: Int,
+                 val descripcion: String
+) {
+    public object Rubros : Table() {
+        val idRubro = integer("idRubro").autoIncrement()
+        val descripcion = varchar("descripcion", 200)
+        override val primaryKey = PrimaryKey(idRubro)
+    }
+}
